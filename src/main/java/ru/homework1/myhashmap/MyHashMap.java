22ba454e2284a876793321184ba9a89ef39bb8ad
dionisis2010 <2010dionisis2010@gmail.com>
2019-10-30 12:07:27 +0300

    стабильная версия

--- src/main/java/ru/homework1/myhashmap/MyHashMap.java
+++ src/main/java/ru/homework1/myhashmap/MyHashMap.java
@@ -2,20 +2,20 @@ package ru.homework1.myhashmap;
 
 public class MyHashMap<K, V> {
     private int count = 0;
-    private int arrayLength;
     private Element[] elements;
+    private int[] hashCodes;
     private int id = -1;
 
     public MyHashMap(int arrayLength) {
-        this.arrayLength=arrayLength;
         this.elements = new Element[arrayLength];
+        this.hashCodes = new int[arrayLength];
     }
-    public MyHashMap(){
+
+    public MyHashMap() {
         this(10);
     }
 
     /**
-     *
      * @param key
      * @param value
      * @return возвращает true если успешно прошла запись в массив, если уже был эллемент с таким key, то презапишет value
@@ -23,11 +23,10 @@ public class MyHashMap<K, V> {
      */
     public boolean put(K key, V value) {
         int id = searchKey(key);
-        if (id != -1){
+        if (id != -1) {
             elements[id].setValue(value);
             return true;
-        }
-        else{
+        } else {
             if (intcrementID() == -1) return false;
             elements[this.id] = new Element((K) key, (V) value);
             count++;
@@ -37,6 +36,7 @@ public class MyHashMap<K, V> {
 
     /**
      * удаление происходит путем присвоения ячейке с указанным key значения null
+     *
      * @param key
      * @return вернет true если удаление прошло успешно
      * вернет false если нет елемента с таким key
@@ -56,9 +56,27 @@ public class MyHashMap<K, V> {
      * выводит в консоль каждый элемент с новой строки
      */
     public void print() {
-        for (int i = 0; i< elements.length; i++){
-            System.out.println(i +". " + elements[i]);
+        for (int i = 0; i < elements.length; i++) {
+            System.out.println(i + ". " + elements[i]);
         }
+    }
+
+    public boolean clear(){
+        for (int i = 0; i< elements.length; i++){
+            elements[i] = null;
+        }
+        this.count=0;
+        this.id = 0;
+        return true;
+    }
+
+    public int size(){
+        return count;
+    }
+
+    public boolean search(K key){
+        if (searchKey(key) != -1) return true;
+        else return false;
     }
 
     /**
@@ -66,19 +84,17 @@ public class MyHashMap<K, V> {
      */
     private int intcrementID() {
         if (count >= elements.length) return -1;
-        for (int id = this.id + 1; id < elements.length; id++) {
-            if (elements[id] == null) {
-                this.id = id;
-                return id;
+        for (int newID = this.id + 1; newID < elements.length; newID++) {
+            if (elements[newID] == null) {
+                return this.id = newID;
             }
         }
         for (int id = 0; id < this.id; id++) {
             if (elements[id] == null) {
-                this.id = id;
-                return id;
+                return this.id = id;
             }
         }
-        return id =-1;
+        return id = -1;
     }
 
     /**
@@ -89,10 +105,7 @@ public class MyHashMap<K, V> {
         int id;
         for (id = 0; id < elements.length; id++) {
             if (elements[id] == null) continue;
-
-            Element element = elements[id];
-            if (element.getKey().equals(key)) return id;
-            else continue;
+            if (elements[id].getKey().equals(key)) return id;
         }
         return -1;
     }
